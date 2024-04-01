38efe39a3e1b38a92b81b7886dfdb5667115eae9
Artemii Kravchuk <artemiykra@gmail.com>
2024-04-01 16:05:04 +0000

    add level borders

---
+++ border.lua
@@ -0,0 +1,12 @@
+require"/dynamic/ppol/.lua"
+require"globals"
+
+meshes = {
+
+  {
+    vertexes = {{0, 0}, {LEVEL_WIDTH_INT, 0}, {LEVEL_WIDTH_INT,LEVEL_HEIGHT_INT}, {0, LEVEL_HEIGHT_INT}},
+    segments = {{0, 1, 2, 3, 0}},
+    colors = {0xffffffff}
+  },
+
+}
--- level.lua
+++ level.lua
@@ -1,1 +1,12 @@
-require'/dynamic/ppol/.lua'
+require"/dynamic/ppol/.lua"
+require"globals"
+
+set_level_size(LEVEL_WIDTH, LEVEL_HEIGHT)
+
+ship = new_player_ship(LEVEL_WIDTH/2fx, LEVEL_HEIGHT/2fx)
+set_player_ship_weapon(1, cannon_frequency["_15"], cannon_type["triple"])
+
+border = new_entity(0fx, 0fx)
+entity_set_mesh(border, "border")
+
+add_memory_print()
