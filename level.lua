6bf792e5ed0ddc5c1a7a80d4d5d22bce04ccb642
Artemii Kravchuk <artemiykra@gmail.com>
2024-04-06 09:44:20 +0000

    nits

--- level.lua
+++ level.lua
@@ -6,6 +6,7 @@ set_level_size(LEVEL_WIDTH, LEVEL_HEIGHT)
 
 ship = new_player_ship(LEVEL_WIDTH/2fx, LEVEL_HEIGHT/2fx)
 set_player_ship_weapon(1, cannon_frequency["_15"], cannon_type["triple"])
+set_shield(3)
 ship_speed = 0.99
 
 border = new_entity(LEVEL_WIDTH/2fx, LEVEL_HEIGHT/2fx)
