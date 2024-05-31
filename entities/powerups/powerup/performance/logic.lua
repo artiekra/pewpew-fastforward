b12bbf48754f35f5b39c7e368ad6ff13a3e9948c
Artemii Kravchuk <artemiykra@gmail.com>
2024-05-31 11:00:37 +0300

    fix typo

--- entities/powerups/powerup/performance/logic.lua
+++ entities/powerups/powerup/performance/logic.lua
@@ -5,13 +5,13 @@ require"entities/powerups/config"
 local module = {}
 
 
-function performance_player_collision(entity_id, player_id, ship_id, multiplyer)
-  set_shield(get_shield() + multiplyer)
+function performance_player_collision(entity_id, player_id, ship_id, multiplier)
+  set_shield(get_shield() + multiplier)
 end
 
 
 -- Spawn entity, add update callback
-function module.spawn(ship_id, x, y, multiplyer)
+function module.spawn(ship_id, x, y, multiplier)
   local colors = {
     {0x008015ff, 0x450044ff, 0x940c00ff, 0x808080ff0},
     {0x008015ff, 0x450044ff, 0x940c00ff, 0x808080ff0},
@@ -19,11 +19,11 @@ function module.spawn(ship_id, x, y, multiplyer)
   }
 
   function inner_box_player_collision(entity_id, player_id, ship_id)
-    performance_player_collision(entity_id, player_id, ship_id, multiplyer)
+    performance_player_collision(entity_id, player_id, ship_id, multiplier)
     entity_destroy(entity_id)
   end
 
-  local text = "x" .. multiplyer .. " performance"
+  local text = "x" .. multiplier .. " performance"
   local outer_box, inner_box = template.spawn(ship_id, x, y, "entities/powerups/powerup/performance/icon",
     text, colors, shield_player_collision)
   entity_set_player_collision(inner_box, inner_box_player_collision)
