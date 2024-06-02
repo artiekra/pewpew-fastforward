e295da68d7a3da54fd5eae94cdda9fbf03c628d3
Artemii Kravchuk <artemiykra@gmail.com>
2024-06-02 13:14:31 +0300

    mbaf code refactor

--- entities/mbaf/logic.lua
+++ entities/mbaf/logic.lua
@@ -1,23 +1,21 @@
 local performance = require"misc/performance"
 local helpers = require"entities/helpers"
+require"entities/mbaf/config"
 
 local module = {}
 
 local entities = {}
 
 
-local radius = 10fx
-local speed = 8fx
-local spin_speed = 0.1024fx
-local colors_light = {0xf9f5f0ff, 0xf5f0f4ff, 0xf4fcf0ff, 0xf0f0f0ff}
-local colors_dark = {0x094500ff, 0x450044ff, 0x940c00ff, 0x808080ff}
-
+-- Declaring scheme for entity table
 local i_type = 1
 local i_time = 2
 local i_angle = 3
 local i_dx = 4
 local i_dy = 5
 
+
+-- Function to call every tick on entity
 local function update_callback(id)
   local e = entities[id]
   if not e then
@@ -25,13 +23,16 @@ local function update_callback(id)
   end
   e[i_time] = e[i_time] + 1
 
-  entity_change_pos(id, e[i_dx] * speed, e[i_dy] * speed)
-  entity_add_mesh_angle(id, spin_speed, e[i_dx], e[i_dy], 0fx)
+  entity_change_pos(id, e[i_dx] * SPEED, e[i_dy] * SPEED)
+  entity_add_mesh_angle(id, SPIN_SPEED, e[i_dx], e[i_dy], 0fx)
 
-  helpers.set_entity_color(e[i_time], id, e[i_type] == 1 and colors_light or colors_dark)
+  helpers.set_entity_color(e[i_time], id, e[i_type] == 1 and COLORS_LIGHT or COLORS_DARK)
 end
 
-local function initial_interpolation_fix(id) -- has to be after update_callback due to local visibility
+
+-- Fixing interpolation at first 2 ticks
+-- has to be after update_callback due to local visibility
+local function initial_interpolation_fix(id)
   local e = entities[id]
   if not e then
     return
@@ -43,6 +44,8 @@ local function initial_interpolation_fix(id) -- has to be after update_callback 
   end
 end
 
+
+-- Set wall collision callback function for the entity
 local function wall_collision(id, wall_normal_x, wall_normal_y)
   local e = entities[id]
   local dot_product_move = ((wall_normal_x * e[i_dx]) + (wall_normal_y * e[i_dy])) * 2fx
@@ -52,6 +55,8 @@ local function wall_collision(id, wall_normal_x, wall_normal_y)
   entity_set_mesh_angle(id, e[i_angle], 0fx, 0fx, 1fx)
 end
 
+
+-- Set player collision callback function for the entity
 local function player_collision(entity_id, player_id, ship_id)
   damage_player_ship(ship_id, 1)
   entity_start_exploding(entity_id, 10)
@@ -59,6 +64,8 @@ local function player_collision(entity_id, player_id, ship_id)
   performance.increase_player_score(1)
 end
 
+
+-- Set weapon collision callback function for the entity
 local function weapon_collision(entity_id, player_index, weapon)
   if weapon == weapon_type.bullet then
     entity_start_exploding(entity_id, 7)
@@ -73,7 +80,7 @@ function module.spawn(baf_type, x, y, angle)
 
   local id = new_entity(x, y)
   entity_start_spawning(id, 0)
-  entity_set_radius(id, radius)
+  entity_set_radius(id, RADIUS)
 
   local dy, dx = fx_sincos(angle)
   entity_set_mesh_angle(id, angle, 0fx, 0fx, 1fx)
