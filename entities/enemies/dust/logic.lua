5de3237db59598052f158a2a66aebe98ca79abe8
Artemii Kravchuk <artemiykra@gmail.com>
2024-06-02 13:39:46 +0300

    dust code refactor

--- entities/enemies/dust/logic.lua
+++ entities/enemies/dust/logic.lua
@@ -6,51 +6,82 @@ local module = {}
 local entities = {}
 
 
+-- Some entity globals
+local speed = 1fx
+local radius = 2fx
+local colors = {0x007a50ff, 0x0248abff,
+  0xd17300ff, 0x808080ff}
+
+
 -- Declaring scheme for entity table
-local i_type = 1
-local i_time = 2
-local i_angle = 3
-local i_dx = 4
-local i_dy = 5
+local i_time = 1
+local i_angle = 2
+local i_dx = 3
+local i_dy = 4
+
+
+-- Function to call every tick on entity
+function update_callback(id)
+  local e = entities[id]
+  if not e then
+    return
+  end
+  e[i_time] = e[i_time] + 1
+
+  entity_change_pos(id, e[i_dx] * speed, e[i_dy] * speed)
+
+  helpers.set_entity_color(e[i_time], id, colors)
+end
+
+
+-- Fixing interpolation at first 2 ticks
+-- has to be after update_callback due to local visibility
+local function initial_interpolation_fix(id)
+  local e = entities[id]
+  if not e then
+    return
+  end
+  e[i_time] = e[i_time] + 1
+  if e[i_time] == 2 then
+    entity_set_update_callback(id, update_callback)
+    entity_set_mesh(id, 'entities/enemies/dust/mesh')
+  end
+end
+
+
+-- Set wall collision callback function for the entity
+function wall_collision(id, wall_normal_x, wall_normal_y)
+  entity_start_exploding(id, 10)
+end
+
+
+-- Set player collision callback function for the entity
+function player_collision(entity_id, player_id, ship_id)
+  damage_player_ship(ship_id, 1)
+  entity_start_exploding(entity_id, 10)
+  entities[entity_id] = nil
+  performance.increase_player_score(1)
+end
 
 
 -- Spawn entity, add update callback
 function module.spawn(x, y, angle)
-  local speed = 1fx
-  local colors = {0x007a50ff, 0x0248abff,
-    0xd17300ff, 0x808080ff}
 
-  local dust = new_entity(x, y)
-  entity_start_spawning(dust, 2)
-  entity_set_mesh(dust, "entities/enemies/dust/mesh")
-  entity_set_radius(dust, 2fx)
+  local id = new_entity(x, y)
+  entity_start_spawning(id, 0)
+  entity_set_mesh(id, "entities/enemies/dust/mesh")
+  entity_set_radius(id, radius)
 
-  local time = 0
   local dy, dx = fx_sincos(angle)
-  function dust_update_callback()
-    time = time + 1
+  entity_set_mesh_angle(id, angle, 0fx, 0fx, 1fx)
 
-    entity_change_pos(dust, dx*speed, dy*speed)
+  entities[id] = {0, angle, dx, dy}
 
-    helpers.set_entity_color(time, dust, colors)
-  end
+  entity_set_update_callback(id, initial_interpolation_fix)
+  entity_set_wall_collision(id, true, wall_collision)
+  entity_set_player_collision(id, player_collision)
 
-  function dust_wall_collision()
-    entity_start_exploding(dust, 10)
-  end
-
-  function dust_player_collision(entity_id, player_id, ship_id)
-    damage_player_ship(ship_id, 1)
-    entity_start_exploding(entity_id, 10)
-
-    performance.increase_player_score(1)
-  end
-
-  entity_set_update_callback(dust, dust_update_callback)
-  entity_set_wall_collision(dust, true, dust_wall_collision)
-  entity_set_player_collision(dust, dust_player_collision)
-
-  return dust
+  return id 
 end
 
 
