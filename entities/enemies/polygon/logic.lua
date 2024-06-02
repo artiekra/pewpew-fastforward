2935d5ed6b8bdf5bdb304169f6eb415d2cde0896
Artemii Kravchuk <artemiykra@gmail.com>
2024-06-02 14:23:36 +0300

    polygon code refactor

--- entities/enemies/polygon/logic.lua
+++ entities/enemies/polygon/logic.lua
@@ -7,12 +7,31 @@ require"entities/enemies/polygon/config"
 
 local module = {}
 
+local entities = {}
+
+-- Some entity globals
+local speed = 2fx
+local radius = 30fx
+local default_health = 5
+
 -- [NOTE: cant have maximum alpha value,
 --  its increased for effect, when hit by bullet]
 local colors = {0x009a6590, 0xa700fd90,
   0xe0340090, 0x80808090}
 
+local explosion_color = colors[1]
 
+-- Declaring scheme for entity table
+local i_time = 1
+local i_angle = 2
+local i_dx = 3
+local i_dy = 4
+local i_health = 5
+local i_highlight = 6
+local i_meshi = 7
+
+
+-- Function to explode the polygon, spawn bullets, etc..
 function module.destroy_polygon(polygon_id, color)
   entity_start_exploding(polygon_id, 15)
   performance.increase_player_score(5)
@@ -30,87 +49,124 @@ function module.destroy_polygon(polygon_id, color)
 end
 
 
--- Spawn entity, add update callback
-function module.spawn(x, y, angle)
-  local speed = 2fx
-  local health = 5
+-- Function to call every tick on entity
+function update_callback()
+  local e = entities[id]
+  if not e then
+    return
+  end
+  e[i_time] = e[i_time] + 1
+  e[i_highlight] = e[i_highlight] - 1
 
-  local explosion_color = colors[1]
+  entity_change_pos(id, e[i_dx] * SPEED, e[i_dy] * SPEED)
 
-  local polygon = new_entity(x, y)
-  entity_start_spawning(polygon, 2)
-  entity_set_mesh(polygon, "entities/enemies/polygon/mesh")
-  entity_set_radius(polygon, 30fx)
+  if e[i_meshi] >= ANIMATION_TIME*60 then
+    e[i_meshi] = 0
+  end
 
-  local time = 0
-  local mesh_index = 0
-  local highlight = 0
-  local dy, dx = fx_sincos(angle)
-  function polygon_update_callback()
-    time = time + 1
-    highlight = highlight - 1
-    
-    entity_change_pos(polygon, dx*speed, dy*speed)
+  entity_set_mesh(id, "entities/enemies/polygon/mesh", e[i_meshi], e[i_mesh_index]+1)
+  e[i_meshi] = e[i_mesh_index] + 2
 
-    if mesh_index >= ANIMATION_TIME*60 then
-      mesh_index = 0
-    end
-
-    entity_set_mesh(polygon, "entities/enemies/polygon/mesh", mesh_index, mesh_index + 1)
-    mesh_index = mesh_index + 2
-
-    local color_state = helpers.get_color_state(time)
-    if color_state then
-      color = colors[color_state]
-      explosion_color = color
-      if highlight > 0 then
-        entity_set_mesh_color(polygon, ch.make_color_with_alpha(color, 255))
-      else
-        entity_set_mesh_color(polygon, color)
-      end
+  local color_state = helpers.get_color_state(e[i_time])
+  if color_state then
+    color = colors[color_state]
+    explosion_color = color
+    if highlight > 0 then
+      entity_set_mesh_color(id, ch.make_color_with_alpha(color, 255))
+    else
+      entity_set_mesh_color(id, color)
     end
   end
 
-  function polygon_wall_collision(entity_id, wall_normal_x, wall_normal_y)
-    local dot_product_move = ((wall_normal_x * dx) + (wall_normal_y * dy)) * 2fx; 
-    dx = dx - (wall_normal_x * dot_product_move)
-    dy = dy - (wall_normal_y * dot_product_move); 
-    angle = fx_atan2(dy,dx)
+end
+
+
+-- Fixing interpolation at first 2 ticks
+-- has to be after update_callback due to local visibility
+local function initial_interpolation_fix(id)
+  local e = entities[id]
+  if not e then
+    return
   end
+  e[i_time] = e[i_time] + 1
+  if e[i_time] == 2 then
+    entity_set_update_callback(id, update_callback)
+    entity_set_mesh(id, "entities/enemies/polygon/mesh")
+  end
+end
 
 
-  function polygon_player_collision(entity_id, player_id, ship_id)
-    if health > 0 then
-      if health > 3 then
-        damage_player_ship(ship_id, 3)
-      else
-        damage_player_ship(ship_id, health)
-      end
-      module.destroy_polygon(entity_id, explosion_color)
+-- Set wall collision callback function for the entity
+function wall_collision(entity_id, wall_normal_x, wall_normal_y)
+  local e = entities[entity_id]
+  local dot_product_move = ((wall_normal_x * e[i_dx]) + (wall_normal_y * e[i_dy])) * 2fx
+  e[i_dx] = e[i_dx] - wall_normal_x * dot_product_move
+  e[i_dy] = e[i_dy] - wall_normal_y * dot_product_move
+  e[i_angle] = fx_atan2(e[i_dy], e[i_dx])
+  entity_set_mesh_angle(id, e[i_angle], 0fx, 0fx, 1fx)
+end
 
-      performance.increase_player_score(3)
+
+-- Set player collision callback function for the entity
+function player_collision(entity_id, player_id, ship_id)
+  local e = entities[entity_id]
+
+  if e then
+    local health = e[i_health]
+
+    if health > 3 then
+      damage_player_ship(ship_id, 3)
+    else
+      damage_player_ship(ship_id, health)
     end
-  end
+    module.destroy_polygon(entity_id, explosion_color)
 
-  function polygon_weapon_collision(entity_id, player_index, weapon)
+    entities[entity_id] = nil
+    performance.increase_player_score(3)
+  end
+end
+
+
+-- Set weapon collision callback function for the entity
+function weapon_collision(entity_id, player_index, weapon)
+  local e = entities[entity_id]
+
+  if e then
+    local health = e[i_health]
+
     if health > 0 then
       if weapon == weapon_type.bullet then
-        health = health - 1
-        highlight = 5
+        e[i_health] = health - 1  -- [TODO: assigning to health, reference to array?]
+        e[i_highlight] = 5
         if health <= 0 then
           module.destroy_polygon(entity_id, explosion_color)
         end
       end
     end
-    return true
   end
 
-  entity_set_update_callback(polygon, polygon_update_callback)
-  entity_set_wall_collision(polygon, true, polygon_wall_collision)
-  entity_set_player_collision(polygon, polygon_player_collision)
-  entity_set_weapon_collision(polygon, polygon_weapon_collision)
+  return true
+end
 
-  return polygon
+
+-- Spawn entity, add update callback
+function module.spawn(x, y, angle)
+
+  local id = new_entity(x, y)
+  entity_start_spawning(id, 2)
+  entity_set_radius(id, radius)
+
+  local dy, dx = fx_sincos(angle)
+  entity_set_mesh_angle(id, angle, 0fx, 0fx, 1fx)
+  
+  entities[id] = {0, angle, dx, dy, default_health, 0, 0}
+
+  entity_set_update_callback(id, initial_interpolation_fix)
+  entity_set_wall_collision(id, true, wall_collision)
+  entity_set_player_collision(id, player_collision)
+  entity_set_weapon_collision(id, weapon_collision)
+
+  return id 
 end
 
 
