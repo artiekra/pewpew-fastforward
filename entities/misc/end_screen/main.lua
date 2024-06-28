eadad33c0f21ea540eceaad5dcf9418378d867fd
Artemii Kravchuk <artemiykra@gmail.com>
2024-06-28 21:27:19 +0300

    end screen - add secondary text

--- entities/misc/end_screen/main.lua
+++ entities/misc/end_screen/main.lua
@@ -21,7 +21,15 @@ function module.show()
 
   local main_label_text = MAIN_LABEL_VARIATIONS[random(1, 3)]
   helpers.create_bold_label(-LEVEL_WIDTH * 2fx/3fx, LEVEL_HEIGHT - 150fx,
-    main_label_text, color, 4fx, 0fx, -TILT_ANGLE*1.2048fx, 6)
+    main_label_text, color, 5.2048fx, 0fx, -TILT_ANGLE*1.2048fx, 7)
+
+  local text2 = "Thanks for playing my level!"
+  helpers.create_bold_label(-LEVEL_WIDTH * 2fx/3fx, LEVEL_HEIGHT - 300fx,
+    text2, 0xffffffff, 2fx, 0fx, -TILT_ANGLE*1.2048fx, 3)
+
+  local text3 = "Here are some bonuses for you:"
+  helpers.create_bold_label(-LEVEL_WIDTH * 2fx/3fx, LEVEL_HEIGHT - 375fx,
+    text3, 0xffffffff, 2fx, 0fx, -TILT_ANGLE*1.2048fx, 3)
 
 end
 
