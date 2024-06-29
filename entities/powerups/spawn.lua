8f5243d78262db20b76b6d004605fc813a4c0a51
Artemii Kravchuk <artemiykra@gmail.com>
2024-06-29 16:29:51 +0300

    fixes

--- entities/powerups/spawn.lua
+++ entities/powerups/spawn.lua
@@ -3,7 +3,7 @@ local shield_pu = require"entities/powerups/powerup/shield/logic"
 local score_pu = require"entities/powerups/powerup/score/logic"
 local performance_pu = require"entities/powerups/powerup/performance/logic"
 
-module = {}
+pu_spawn_module = {}
 
 -- How far away can powerups spawn from:
 -- 1. Player ship (square around ship is blocked)
@@ -16,7 +16,7 @@ POWERUP_SPAWN_OFFSETS = {
 
 
 -- Get random coordinates and spawn shield powerup
-function module.spawn_shield_pu(ship)
+function pu_spawn_module.spawn_shield_pu(ship)
   local x, y = helpers.random_coordinates(
     ship, table.unpack(POWERUP_SPAWN_OFFSETS.shield))
   local powerup_outer, powerup_inner = shield_pu.spawn(ship, x, y)
@@ -26,7 +26,7 @@ end
 
 
 -- Get random coordinates and spawn score powerup
-function module.spawn_score_pu(ship)
+function pu_spawn_module.spawn_score_pu(ship)
   local x, y = helpers.random_coordinates(
     ship, table.unpack(POWERUP_SPAWN_OFFSETS.score))
   local powerup_outer, powerup_inner = score_pu.spawn(ship, x, y)
@@ -36,7 +36,7 @@ end
 
 
 -- Get random coordinates and spawn performance powerup
-function module.spawn_performance_pu(ship)
+function pu_spawn_module.spawn_performance_pu(ship)
   local x, y = helpers.random_coordinates(
     ship, table.unpack(POWERUP_SPAWN_OFFSETS.performance))
   local powerup_outer, powerup_inner = performance_pu.spawn(
@@ -48,14 +48,15 @@ end
 
 -- Chance table for powerups (after preconditions)
 POWERUP_SPAWN_WEIGHTS = {
-  {100, module.spawn_shield_pu}, {100, module.spawn_score_pu},
-  {100, module.spawn_performance_pu}
+  {100, pu_spawn_module.spawn_shield_pu},
+  {100, pu_spawn_module.spawn_score_pu},
+  {100, pu_spawn_module.spawn_performance_pu}
 }
 
 
 -- Function for getting weighted random numbers
 -- https://www.reddit.com/r/lua/comments/8t0mlf/methods_for_weighted_random_picks/
-function module.weighted_random(pool)
+function pu_spawn_module.weighted_random(pool)
 
   local poolsize = 0
   for k,v in pairs(pool) do
@@ -74,7 +75,7 @@ end
 
 
 -- https://www.reddit.com/r/lua/comments/mcen99/how_to_deal_with_lua_tables_copies_and_not_mess/
-function module.shallow_table_copy(maintable)
+function pu_spawn_module.shallow_table_copy(maintable)
 
   local tablecopy = {}
   for k, v in pairs(maintable) do
@@ -86,27 +87,28 @@ end
 
 
 -- Uses complex conditions to spawn the correct powerup
-function module.spawn_powerup(ship, time)
+function pu_spawn_module.spawn_powerup(ship, time)
 
   -- If player is low on shields, spawn shield powerup
   -- (100% for <2 shields, 50% for <3 shields)
   player_shields = get_shield()
   if player_shields < 2 then
-    module.spawn_shield_pu(ship)
-    return
+    local powerup = pu_spawn_module.spawn_shield_pu(ship)
+    return powerup
   elseif player_shields < 3 then
     if random(0, 1) then
-      module.spawn_shield_pu(ship)
-      return
+      local powerup = pu_spawn_module.spawn_shield_pu(ship)
+      return powerup
     end
   end
 
   -- No preconditions satisfied, spawn random powerup
   -- (according to chance table)
-  local choice = module.weighted_random(POWERUP_SPAWN_WEIGHTS)
+  local choice = pu_spawn_module.weighted_random(POWERUP_SPAWN_WEIGHTS)
   powerup = choice(ship)
-
+  
   return powerup
 end
 
-return module
+
+return pu_spawn_module
--- globals/end_screen.lua
+++ globals/end_screen.lua
@@ -1,4 +1,4 @@
-LEVEL_DURATION_TICKS = 100
+LEVEL_DURATION_TICKS = 18000  -- 10 minutes
 IS_END_SCREEN = false  -- mutable by level components variable
 END_SCREEN_ENTITY_COLOR = 0x252525ff
 
