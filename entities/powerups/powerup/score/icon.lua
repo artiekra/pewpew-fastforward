c0e3bc6202dd81799b9de089bd09071badd25f40
Artemii Kravchuk <artemiykra@gmail.com>
2024-07-17 23:38:17 +0300

    unique icon for score powerup

--- entities/powerups/powerup/score/icon.lua
+++ entities/powerups/powerup/score/icon.lua
@@ -1,15 +1,13 @@
--- [TODO: make unique icon]
-
 r = 22
-b = r/2.5
-a = r/5
+b = r/3
+z = 0
 
 meshes = {
   {
     vertexes = {
-      {b, -a}, {b, a}, {a, a}, {a, b}, {-a, b}, {-a, a}, {-b, a}, {-b, -a}, {-a, -a}, {-a, -b},
-      {a, -b}, {a, -a},
+      {-b, -b*1/2-b/4, z}, {0, b*3/4, z}, {b, -b*1/2-b/4, z}
     },
-    segments = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0}}
+    segments = {{0, 1, 2, 0}}
   }
 }
+
--- entities/powerups/powerup/score/logic.lua
+++ entities/powerups/powerup/score/logic.lua
@@ -7,7 +7,7 @@ local module = {}
 
 function points_player_collision(entity_id, player_id, ship_id)
   -- [NOTE: consider not accounting from performance here?]
-  performance.increase_player_score(10)
+  performance.increase_player_score(1000)
 end
 
 
@@ -20,7 +20,7 @@ function module.spawn(ship_id, x, y)
   }
 
   local outer_box, inner_box = template.spawn(ship_id, x, y, "entities/powerups/powerup/score/icon",
-    "+10 points", colors, points_player_collision)
+    "+1K points", colors, points_player_collision)
 
   return outer_box, inner_box
 end
